f5032e3d148d81dbc19e58417b95f30f5abd3321
Adriano Pereira Reis <39317082+areistte@users.noreply.github.com>
2019-02-09 10:43:11 -0200

    Endereco
    Teste

---
+++ Api.Ado.NetCore/Models/Endereco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Ado.NetCore.Models
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+    }
+}
